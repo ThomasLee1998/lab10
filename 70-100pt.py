384a40b4da44c3d5dbbd4f8a8595b882bb128c02
Thomas (Tommy) Lee Hartnagel <tommyleehartnagel@gmail.com>
2014-10-21 14:10:53 -0500

    100 done

--- 70-100pt.py
+++ 70-100pt.py
@@ -25,16 +25,18 @@ drawpad.grid(row=0, column=1)
 
 
 # my house with my roof
-house = drawpad.create_rectangle(300,300,400,400)
+house = drawpad.create_rectangle(300,300,400,400,fill="red")
 roof1 = drawpad.create_line(300,300,350,200)
 roof2 = drawpad.create_line(400,300,350,200)
 #windows+door
-window=drawpad.create_rectangle(310,310,340,340)
-window2=drawpad.create_rectangle(360,310,390,340)
-door= drawpad.create_rectangle(340,360,360,400)
+window=drawpad.create_rectangle(310,310,340,340,fill="white")
+window2=drawpad.create_rectangle(360,310,390,340,fill="white")
+door= drawpad.create_rectangle(340,360,360,400,fill="brown")
 #chimney and door knob
-doorknob= drawpad.create_oval(350,380,360,370)
+doorknob= drawpad.create_oval(350,380,360,370,fill="yellow")
 chimney = drawpad.create_line(300,300,300,200)
 chimney1 = drawpad.create_line(320,260,320,200)
 chimney1 = drawpad.create_line(300,200,320,200)
+#green grass red house
+grass = drawpad.create_rectangle(300,400,400,500,fill="green")
 root.mainloop()
